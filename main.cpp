948689f0de441e4e1bbe74d998f23861a8353dc4
khs4510 <rlagudtn4510@naver.com>
2020-11-02 00:39:49 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include "solution.h"
+using namespace std;
+
+Solution s;
+
+/* This main function is only for reference. 
+ * Evaluation will be made by other main function tailored for grading 
+ * Please, DO NOT submit main.cpp and solution.h files 
+ */ 
+
+int main (void)
+{
+    struct TreeNode* root;
+    unsigned long long int answer;
+    root = s.buildTree("sample1.txt");
+    
+    /* Run your codes */
+    clock_t begin = clock();    
+    answer = s.getPathSum(root);
+    clock_t end = clock();
+
+    cout << answer << endl;
+    cout << "Elapsed time: " << double (end - begin)/CLOCKS_PER_SEC << " (s)" << endl;
+
+    return 0;
+}
---
+++ solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "solution.h"
+using namespace std;
+
+/* Fill free to implement any function on here */
+
+unsigned long long int Solution::getPathSum (struct TreeNode* root)
+{
+    /* Fill up with your own codes */
+}
+
+struct TreeNode* Solution::buildTree (string filename)
+{
+    /* Fill up with your own codes */
+
+}
